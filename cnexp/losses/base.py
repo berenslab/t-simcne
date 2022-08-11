06993ea73d25d52d816ad9d5dab1543d21ad4673
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-08-11 17:32:40 +0200

    Revert saving of loss state dict
    Needs some parameters that are missing in other modules.  I don't want
    to mess with that currently.

--- cnexp/losses/base.py
+++ cnexp/losses/base.py
@@ -8,6 +8,15 @@ class LossBase(ProjectBase):
     def __init__(self, path, random_state=None, **kwargs):
         super().__init__(path, random_state=random_state)
         self.kwargs = kwargs
+        # parameters are requires for saving as a state dict.  Not
+        # sure how that could influence things down the line so I am
+        # only saving the loss object itself for now.
+        #
+        # self._parameters = kwargs
+        # self._buffers = dict()
+        # self._modules = dict()
+        # self._state_dict_hooks = dict()
+
     def get_deps(self):
         return []
 
@@ -24,7 +33,7 @@ class LossBase(ProjectBase):
         save_data = dict(
             **self.state_dict,
             criterion=self.criterion,
-            criterion_sd=self.criterion.state_dict(),
+            # criterion_sd=self.criterion.state_dict(),
         )
         self.save_lambda_alt(self.outdir / "model.pt", save_data, torch.save)
 
