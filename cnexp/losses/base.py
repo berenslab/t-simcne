dff38990ba9b3b965788f11c8589152136009e57
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-08-12 08:29:43 +0200

    Save loss func in criterion.pt instead of model.pt
    The loss function does not interact or depend on anything in the
    model (i.e. does not have a closure over the model params).  So it's
    easier to save it in a different file.
    
    (That could also make it easier for KL-style training.)

--- cnexp/losses/base.py
+++ cnexp/losses/base.py
@@ -9,22 +9,18 @@ class LossBase(ProjectBase):
         super().__init__(path, random_state=random_state)
         self.metric = metric
         self.kwargs = kwargs
+
     def get_deps(self):
         return []
 
     def load(self):
-        if (self.indir / "model.pt").exists():
-            self.state_dict = torch.load(self.indir / "model.pt")
-        else:
-            self.state_dict = dict()
+        pass
 
     def save(self):
-        self.state_dict.pop("criterion", None)
-        self.state_dict.pop("criterion_sd", None)
-
         save_data = dict(
-            **self.state_dict,
             criterion=self.criterion,
             criterion_sd=self.criterion.state_dict(),
         )
-        self.save_lambda_alt(self.outdir / "model.pt", save_data, torch.save)
+        self.save_lambda_alt(
+            self.outdir / "criterion.pt", save_data, torch.save
+        )
