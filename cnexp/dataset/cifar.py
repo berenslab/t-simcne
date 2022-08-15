965b83075619ffd20bb1d216f2cbf6f64b07e238
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-08-15 17:51:36 +0200

    Move cifar loading to a more generic function
    Now I can load torchvision datasets more generally with a helper
    function in another file.

--- cnexp/dataset/cifar.py
+++ cnexp/dataset/cifar.py
@@ -1,53 +1,48 @@
 import contextlib
 import inspect
 
-import torch
 from torchvision import datasets as tvdatasets
 
 from ..imagedistortions import TransformedPairDataset, get_transforms
 from .base import DatasetBase
+from .load_torchvision_dataset import load_torchvision_dataset
 
 
-def load_cifar10(download=True, **kwargs):
+def load_cifar10(**kwargs):
     # hardcode dataset mean and std
     mean = (0.4914, 0.4822, 0.4465)
     std = (0.2023, 0.1994, 0.2010)
+    size = (32, 32)
 
-    transform = get_transforms(mean, std, size=(32, 32), setting="contrastive")
-
-    dataset = tvdatasets.CIFAR10(
-        download=download,
-        **kwargs,
+    return load_torchvision_dataset(
+        tvdatasets.CIFAR10, mean, std, size, **kwargs
     )
 
-    # need to make a dataset that returns two transforms of an image
-    return TransformedPairDataset(dataset, transform)
 
-
-def load_cifar100(download=True, **kwargs):
+def load_cifar100(download=True, train=None, **kwargs):
     mean = (0.5071, 0.4867, 0.4408)
     std = (0.2675, 0.2565, 0.2761)
+    size = (32, 32)
 
-    transform = get_transforms(mean, std, size=(32, 32), setting="contrastive")
-
-    dataset = tvdatasets.CIFAR100(
-        download=download,
-        **kwargs,
+    return load_torchvision_dataset(
+        tvdatasets.CIFAR100, mean, std, size, **kwargs
     )
-
-    # need to make a dataset that returns two transforms of an image
-    return TransformedPairDataset(dataset, transform)
 
 
 class CIFAR10(DatasetBase):
     def get_deps(self):
         supdeps = super().get_deps()
-        return supdeps + [inspect.getfile(TransformedPairDataset)]
+        deps = [
+            inspect.getfile(get_transforms),
+            inspect.getfile(load_torchvision_dataset),
+            inspect.getfile(TransformedPairDataset),
+        ]
+        return supdeps + deps
 
     def compute(self):
         with open(self.outdir / "stdout.txt", "w") as f:
             with contextlib.redirect_stdout(f):
-                self.dataset = load_cifar10(
+                self.data_sd = load_cifar10(
                     root=self.outdir / "cifar10", **self.kwargs
                 )
 
@@ -56,6 +51,6 @@ class CIFAR100(CIFAR10):
     def compute(self):
         with open(self.outdir / "stdout.txt", "w") as f:
             with contextlib.redirect_stdout(f):
-                self.dataset = load_cifar100(
+                self.data_sd = load_cifar100(
                     root=self.outdir / "cifar100", **self.kwargs
                 )
