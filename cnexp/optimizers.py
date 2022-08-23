c55eee624c4360aa6aa1c9dd856cc9f637ed0fc3
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-08-23 16:35:23 +0200

    Change default lr to be inferred from batch_size
    The optimizer now accepts an argument (also the default) that will try
    to inspect a dataloader and use a linear scale of the batch size (as
    written in the simclr paper).
    
    Should maybe rename to something shorter.  And add square root scaling.

--- cnexp/optimizers.py
+++ cnexp/optimizers.py
@@ -5,18 +5,34 @@ from .base import ProjectBase
 
 
 class OptimBase(ProjectBase):
-    def __init__(self, path, random_state=None, **kwargs):
+    def __init__(
+        self, path, random_state=None, lr="scale-lin-batchsize", **kwargs
+    ):
         super().__init__(path, random_state=random_state)
         # kwargs.setdefault("momentum", 0.9)
         # kwargs.setdefault("weight_decay", 5e-4)
+        self.lr = lr
         self.kwargs = kwargs
 
     def get_deps(self):
-        return [self.indir / "model.pt"]
+        if self.lr == "scale-lin-batchsize":
+            extra_dep = [self.indir / "dataset.pt"]
+        else:
+            extra_dep = []
+
+        return [self.indir / "model.pt"] + extra_dep
 
     def load(self):
         self.state_dict = torch.load(self.indir / "model.pt")
         self.model = self.state_dict["model"]
+
+        if self.lr == "scale-lin-batchsize":
+            loader = torch.load(self.indir / "dataset.pt")[
+                "train_contrastive_loader"
+            ]
+            self.batch_size = loader.batch_size
+        else:
+            self.batch_size = None
 
     def save(self):
         # remove old values that might be present
@@ -31,7 +47,20 @@ class OptimBase(ProjectBase):
         self.save_lambda_alt(self.outdir / "model.pt", save_data, torch.save)
 
 
-def make_sgd(model, lr=0.12, momentum=0.9, weight_decay=5e-4, **kwargs):
+def lr_from_batchsize(batch_size: int, /) -> float:
+    return 0.03 * batch_size / 256
+
+
+def make_sgd(
+    model, lr=0.12, momentum=0.9, weight_decay=5e-4, batch_size=None, **kwargs
+):
+    if batch_size is not None and lr == "scale-lin-batchsize":
+        lr = lr_from_batchsize(batch_size)
+    elif isinstance(lr, (int, float)):
+        lr = lr
+    else:
+        raise ValueError(f"No valid {lr = } or {batch_size = } passed")
+
     return optim.SGD(
         model.parameters(),
         lr=lr,
@@ -43,10 +72,21 @@ def make_sgd(model, lr=0.12, momentum=0.9, weight_decay=5e-4, **kwargs):
 
 class SGD(OptimBase):
     def compute(self):
-        self.opt = make_sgd(self.model, **self.kwargs)
+        self.opt = make_sgd(
+            self.model, lr=self.lr, batch_size=self.batch_size, **self.kwargs
+        )
 
 
-def make_adam(model, lr=0.12, momentum=0.9, weight_decay=5e-4, **kwargs):
+def make_adam(
+    model, lr=0.12, momentum=0.9, weight_decay=5e-4, batch_size=None, **kwargs
+):
+    if batch_size is not None and lr == "scale-lin-batchsize":
+        lr = lr_from_batchsize(batch_size)
+    elif isinstance(lr, (int, float)):
+        lr = lr
+    else:
+        raise ValueError(f"No valid {lr = } or {batch_size = } passed")
+
     return optim.Adam(
         model.parameters(),
         lr=lr,
@@ -58,4 +98,6 @@ def make_adam(model, lr=0.12, momentum=0.9, weight_decay=5e-4, **kwargs):
 
 class Adam(OptimBase):
     def compute(self):
-        self.opt = make_adam(self.model, **self.kwargs)
+        self.opt = make_adam(
+            self.model, lr=self.lr, batch_size=self.batch_size, **self.kwargs
+        )
