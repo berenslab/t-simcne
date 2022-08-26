5f69a2cfa3f08c29762f155365abe59edf86fe1b
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-08-26 18:19:01 +0200

    Fix minor issues in names
    print strings with !s, add some parameters, fix issues with dir seps.

--- cnexp/names.py
+++ cnexp/names.py
@@ -16,21 +16,27 @@ def lin_aug(n_classes="infer", n_epochs=100):
     )
 
 
-def model_opt_lr(out_dim=128, n_epochs=1000):
+def model_opt_lr(out_dim=128, n_epochs=1000, backbone="resnet18"):
     model = "model" if out_dim == 128 else f"model:{out_dim=}"
+    if backbone != "resnet18":
+        model += f":{backbone=!s}"
     lr = "lrcos" if n_epochs == 1000 else f"lrcos:{n_epochs}"
 
-    return model / "sgd" / lr
+    return Path(model) / "sgd" / lr
 
 
-def default_train(*, metric="euclidean", **kwargs):
+def default_train(*, metric="cosine", **kwargs):
     model_etc = model_opt_lr(**kwargs)
-    loss = "infonce" if metric == "euclidean" else f"infonce:{metric=}"
+    loss = "infonce" if metric == "euclidean" else f"infonce:{metric=!s}"
 
     return model_etc / loss / "train"
 
 
-def finetune(llin_epochs=50, ft_epochs=450, freeze_last_stage=0):
+def simclr_train(**kwargs):
+    return default_train(**kwargs)
+
+
+def finetune(llin_epochs=50, ft_epochs=450, ft_lr=1.2e-4, freeze_last_stage=0):
     last_linear_train = Path(
         "ftmodel:freeze=1:change=lastlin/sgd/"
         f"lrcos:n_epochs={llin_epochs}:warmup_epochs=0/infonce/train"
@@ -39,7 +45,8 @@ def finetune(llin_epochs=50, ft_epochs=450, freeze_last_stage=0):
     if freeze_last_stage not in [0, "backbone"]:
         raise ValueError(f"Unknown value for {freeze_last_stage = !r}")
     finetune_train = (
-        last_linear_train / f"ftmodel:freeze={freeze_last_stage}"
-        f"sgd/lrcos:n_epochs={ft_epochs}/train"
+        last_linear_train
+        / f"ftmodel:freeze={freeze_last_stage}"
+        / f"sgd:lr={ft_lr}/lrcos:n_epochs={ft_epochs}/train"
     )
     return finetune_train
