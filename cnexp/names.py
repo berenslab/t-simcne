180e0804e16825e07bcbd6af74b34b74256313b8
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-09-01 14:12:19 +0200

    Add train_kwargs
    This way I can use a different gpu device

--- cnexp/names.py
+++ cnexp/names.py
@@ -16,37 +16,55 @@ def lin_aug(n_classes="infer", n_epochs=100):
     )
 
 
+def format_train(kwargs):
+    train = "train"
+    if kwargs is not None:
+        for k, v in kwargs.items():
+            train += f":{k}={v}"
+    return train
+
+
 def model_opt_lr(out_dim=128, n_epochs=1000, backbone="resnet18"):
     model = "model" if out_dim == 128 else f"model:{out_dim=}"
     if backbone != "resnet18":
         model += f":{backbone=!s}"
-    lr = "lrcos" if n_epochs == 1000 else f"lrcos:{n_epochs}"
+    lr = "lrcos" if n_epochs == 1000 else f"lrcos:{n_epochs=}"
 
     return Path(model) / "sgd" / lr
 
 
-def default_train(*, metric="cosine", **kwargs):
+def default_train(*, metric="cosine", train_kwargs=None, **kwargs):
     model_etc = model_opt_lr(**kwargs)
     loss = "infonce" if metric == "euclidean" else f"infonce:{metric=!s}"
 
-    return model_etc / loss / "train"
+    return model_etc / loss / format_train(train_kwargs)
 
 
 def simclr_train(**kwargs):
     return default_train(**kwargs)
 
 
-def finetune(llin_epochs=50, ft_epochs=450, ft_lr=1.2e-4, freeze_last_stage=0):
-    last_linear_train = Path(
+def finetune(
+    llin_epochs=50,
+    ft_epochs=450,
+    ft_lr=1.2e-4,
+    freeze_last_stage=0,
+    train_kwargs=None,
+):
+    last_linear = Path(
         "ftmodel:freeze=1:change=lastlin/sgd/"
-        f"lrcos:n_epochs={llin_epochs}:warmup_epochs=0/infonce/train"
+        f"lrcos:n_epochs={llin_epochs}:warmup_epochs=0/infonce"
     )
+    train = format_train(train_kwargs)
+
+    last_linear_train = last_linear / train
 
     if freeze_last_stage not in [0, "backbone"]:
         raise ValueError(f"Unknown value for {freeze_last_stage = !r}")
     finetune_train = (
         last_linear_train
         / f"ftmodel:freeze={freeze_last_stage}"
-        / f"sgd:lr={ft_lr}/lrcos:n_epochs={ft_epochs}/train"
+        / f"sgd:lr={ft_lr}/lrcos:n_epochs={ft_epochs}"
+        / train
     )
     return finetune_train
