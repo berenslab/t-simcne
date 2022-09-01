9088ca19eb4c9328fece966d8cfeeac532986584
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-09-01 14:15:23 +0200

    Change default metadata function
    I don't think I need to track the redo version (I sincerely hope so at
    least).

--- cnexp/plot/__init__.py
+++ cnexp/plot/__init__.py
@@ -12,33 +12,9 @@ def get_default_metadata():
         encoding="utf-8",
         capture_output=True,
     )
-    goredo_proc = subprocess.run(
-        ["redo", "-version"],
-        encoding="utf-8",
-        capture_output=True,
-    )
-    pyredo_proc = subprocess.run(
-        ["redo", "--version"],
-        encoding="utf-8",
-        capture_output=True,
-    )
 
     if git_proc.returncode == 0:
         creator_str = f"cnexp {git_proc.stdout.strip()}"
-
-        if goredo_proc.returncode == 0:
-            # goredo 1.27.0 built with go1.19
-            redo_v = goredo_proc.stdout.split()[1]
-            redo_str = f"goredo v{redo_v}"
-        elif pyredo_proc.returncode == 0:
-            # 0.42
-            redo_v = pyredo_proc.stdout.strip()
-            redo_str = f"apenwarr redo v{redo_v}"
-        else:
-            redo_str = ""
-
-        if redo_str != "":
-            creator_str += f", {redo_str}"
 
         meta["Creator"] = creator_str
 
