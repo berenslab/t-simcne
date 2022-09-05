adeb40ed846b22f8ef7b75a4a9bde74ebce3a742
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-09-05 14:51:15 +0200

    Remove seed/random_state from dataloader
    If the generator is fixed with a random seed, then we cannot pickle
    the dataloader anymore:
        TypeError: cannot pickle 'torch._C.Generator' object
    
    There seems to be an open issue abouth this [1] on GitHub but there
    has been hardly any progress recently.
    
    [1]: https://github.com/pytorch/pytorch/issues/43672

--- cnexp/dataset/dataloader.py
+++ cnexp/dataset/dataloader.py
@@ -8,7 +8,6 @@ from .base import DatasetBase
 
 def make_dataloader(
     dataset,
-    seed=None,
     batch_size: int = 1024,
     shuffle: bool = True,
     num_workers: int = -1,
@@ -23,22 +22,17 @@ def make_dataloader(
         # actually degrades.
         num_workers = max(num_workers + os.cpu_count() + 1, 8)
 
-    if seed is not None:
-        gen = torch.Generator().manual_seed(int(seed))
-    else:
-        gen = None
     dataloader = DataLoader(
         dataset,
         batch_size=batch_size,
         shuffle=shuffle,
         num_workers=num_workers,
-        generator=gen,
         **kwargs,
     )
     return dataloader
 
 
-def make_dataloaders(datasets: dict, random_state=None, **kwargs) -> dict:
+def make_dataloaders(datasets: dict, **kwargs) -> dict:
     """Takes in a dict of Datasets and returns a dict of DataLoaders.
 
     The keys in the return dict will be the keys of the original dict
@@ -48,8 +42,6 @@ def make_dataloaders(datasets: dict, random_state=None, **kwargs) -> dict:
     ----------
     datasets : dict of torch.utils.data.Dataset
         Datasets that will be wrapped with a DataLoader.
-    random_state : numpy random generator or None
-        Will be used to generate the seeds for the DataLoader.
     **kwargs : any
         Will be passed on to `make_dataloader`.
 
@@ -61,11 +53,6 @@ def make_dataloaders(datasets: dict, random_state=None, **kwargs) -> dict:
     loaders = dict()
 
     for l_key, data_key in keys:
-        seed = (
-            None
-            if random_state is None
-            else random_state.integers(-(2**63), 2**63 - 1)
-        )
 
         if data_key.startswith("test_") or data_key == "full_plain":
             shuffle = False
@@ -73,7 +60,7 @@ def make_dataloaders(datasets: dict, random_state=None, **kwargs) -> dict:
             shuffle = True
 
         loaders[l_key] = make_dataloader(
-            datasets[data_key], seed=seed, shuffle=shuffle, **kwargs
+            datasets[data_key], shuffle=shuffle, **kwargs
         )
 
     return loaders
@@ -88,9 +75,7 @@ class GenericDataLoader(DatasetBase):
         self.state_dict = torch.load(self.indir / "dataset.pt")
 
     def compute(self):
-        self.dataloader_dict = make_dataloaders(
-            self.state_dict, random_state=self.random_state, **self.kwargs
-        )
+        self.dataloader_dict = make_dataloaders(self.state_dict, **self.kwargs)
 
     def save(self):
         save_data = dict(**self.state_dict, **self.dataloader_dict)
