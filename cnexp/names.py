62c44ae7709d2eefee3b83460d344e1c7db5953e
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-09-10 13:21:53 +0200

    Add random_state option to names formatting

--- cnexp/names.py
+++ cnexp/names.py
@@ -25,11 +25,20 @@ def format_train(kwargs):
 
 
 def model_opt_lr(
-    out_dim=128, n_epochs=1000, warmup_epochs=10, backbone="resnet18"
+    out_dim=128,
+    n_epochs=1000,
+    warmup_epochs=10,
+    backbone="resnet18",
+    random_state=None,
 ):
-    model = "model" if out_dim == 128 else f"model:{out_dim=}"
+    model = "model"
+    if out_dim != 128:
+        model += f":{out_dim=}"
+    if random_state is not None:
+        model += f":{random_state=}"
     if backbone != "resnet18":
         model += f":{backbone=!s}"
+
     lr = "lrcos"
     if n_epochs != 1000:
         lr += ":{n_epochs=}"
@@ -40,8 +49,20 @@ def model_opt_lr(
 
 
 def default_train(
-    *, metric="euclidean", train_kwargs=None, loss="infonce", **kwargs
+    *,
+    metric="euclidean",
+    train_kwargs=None,
+    loss="infonce",
+    random_state=None,
+    **kwargs,
 ):
+    if random_state is not None:
+        kwargs["random_state"] = random_state
+        if train_kwargs is not None:
+            train_kwargs["random_state"] = random_state
+        else:
+            train_kwargs = dict(random_state=random_state)
+
     model_etc = model_opt_lr(**kwargs)
     loss = "infonce" if metric == "euclidean" else f"{loss}:{metric=!s}"
 
@@ -58,12 +79,15 @@ def finetune(
     ft_lr=1.2e-4,
     ft_loss="infonce",
     freeze_last_stage=0,
+    random_state=None,
     train_kwargs=None,
 ):
     last_linear = Path(
         "ftmodel:freeze=1:change=lastlin/sgd/"
         f"lrcos:n_epochs={llin_epochs}:warmup_epochs=0/{ft_loss}"
     )
+    if random_state is not None and train_kwargs is not None:
+        train_kwargs["random_state"] = random_state
     train = format_train(train_kwargs)
 
     last_linear_train = last_linear / train
