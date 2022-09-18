d7930b4c437e33035a6cbeb0f86ac19e3c572e66
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-09-18 16:05:28 +0200

    Add script to launch simple experiments

---
+++ stats/fixup/fix-runs.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from cnexp import redo
+
+
+def main():
+    crashed_runs = {
+        # "ft-3": dict(
+        #     path="../../experiments/cifar/dl/model:random_state=3118/"
+        #     "sgd/lrcos/infonce/train:random_state=3118"
+        #     "/ftmodel:freeze=1:change=lastlin:random_state=3118"
+        #     "/sgd/lrcos:n_epochs=50:warmup_epochs=0/infonce"
+        #     "/train/ftmodel:freeze=0/sgd:lr=0.00012/lrcos:n_epochs=450/train"
+        #     "/default.run",
+        #     time="24:00:00",
+        # ),
+        # "euc.2d-3": dict(
+        #     path="../../experiments/cifar/dl/model:out_dim=2:random_state=3118"
+        #     "/sgd/lrcos/infonce/train:random_state=3118/default.run",
+        #     time="24:00:00",
+        # ),
+        # "euc-c100": dict(
+        #     path="../../experiments/cifar100/dl/model:out_dim=2/sgd/lrcos/"
+        #     "infonce/train/intermediates.zip",
+        #     time="24:00:00",
+        # ),
+        # "supervised": dict(
+        #     path="../../experiments/cifar/dl/model:out_dim=10/sgd/lrcos/"
+        #     "ce_loss/train/intermediates.zip",
+        #     time="24:00:00",
+        # )
+        "budget500-2": dict(
+            path="experiments/cifar/dl/model:random_state=704/"
+            "sgd/lrcos:n_epochs=400/infonce/train:random_state=704/"
+            "ftmodel:freeze=1:change=lastlin:random_state=704/"
+            "sgd/lrcos:n_epochs=25:warmup_epochs=0/infonce/train/default.run",
+            time="24:00:00",
+        )
+    }
+
+    redo.redo_ifchange_slurm(
+        [v["path"] for v in crashed_runs.values()],
+        name=[k for k in crashed_runs.keys()],
+        time_str=[v["time"] for v in crashed_runs.values()],
+        partition="gpu-2080ti",
+    )
+
+
+if __name__ == "__main__":
+    main()
