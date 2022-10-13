ad636fdd40e32d7fed74e9436d03fbc3dc937f7f
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-10-13 14:12:13 +0200

    Add more fixup runs

--- stats/fixup/fix-runs.py
+++ stats/fixup/fix-runs.py
@@ -24,18 +24,34 @@ def main():
         #     "infonce/train/intermediates.zip",
         #     time="24:00:00",
         # ),
-        # "supervised": dict(
-        #     path="../../experiments/cifar/dl/model:out_dim=10/sgd/lrcos/"
-        #     "ce_loss/train/intermediates.zip",
-        #     time="24:00:00",
-        # )
-        "budget500-2": dict(
-            path="experiments/cifar/dl/model:random_state=704/"
-            "sgd/lrcos:n_epochs=400/infonce/train:random_state=704/"
-            "ftmodel:freeze=1:change=lastlin:random_state=704/"
-            "sgd/lrcos:n_epochs=25:warmup_epochs=0/infonce/train/default.run",
+        "supervised": dict(
+            path="../../experiments/cifar100/dl/model:out_dim=100/sgd/lrcos/"
+            "ce_loss/train/intermediates.zip",
             time="24:00:00",
-        )
+        ),
+        # "budget500-2": dict(
+        #     path="../../experiments/cifar/dl/model:random_state=704/"
+        #     "sgd/lrcos:n_epochs=400/infonce/train:random_state=704/"
+        #     "ftmodel:freeze=1:change=lastlin:random_state=704/"
+        #     "sgd/lrcos:n_epochs=25:warmup_epochs=0/infonce/train/default.run",
+        #     time="24:00:00",
+        # ),
+        # "ince-2-1-fixup": dict(
+        #     path="../../experiments/cifar/dl/model/sgd/lrcos/"
+        #     "closs:loss_mode=infonce:negative_samples=2/train/default.run",
+        #     time="24:00:00",
+        # ),
+        # "nce-2-1-fixup": dict(
+        #     path="../../experiments/cifar/dl/model/sgd/lrcos/"
+        #     "closs:loss_mode=nce:negative_samples=2/train/default.run",
+        #     time="24:00:00",
+        # ),
+        # "neg-128-3-fixup": dict(
+        #     path="../../experiments/cifar/dl/model:random_state=3118/"
+        #     "sgd/lrcos/closs:loss_mode=neg_sample:negative_samples=128/"
+        #     "train:random_state=3118/default.run",
+        #     time="24:00:00",
+        # ),
     }
 
     redo.redo_ifchange_slurm(
