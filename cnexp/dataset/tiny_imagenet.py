b238bc4d74e2dc2ef97b685589cc1cdc6b9dcef1
Niklas Böhm <jan-niklas.boehm@uni-tuebingen.de>
2022-10-17 16:24:53 +0200

    Fix tiny imagenet data loader

--- cnexp/dataset/tiny_imagenet.py
+++ cnexp/dataset/tiny_imagenet.py
@@ -1,13 +1,12 @@
 import contextlib
 import hashlib
 import inspect
+import shutil
 import subprocess
 import zipfile
 
 from torch.utils.data import ConcatDataset
 from torchvision.datasets import ImageFolder
-
-import medmnist
 
 from ..imagedistortions import TransformedPairDataset, get_transforms
 from .base import DatasetBase
@@ -39,15 +38,38 @@ def get_imagenet(root):
     if imagenet_dir.exists():
         return imagenet_dir
     else:
-        url = f"http://cs231n.stanford.edu/{fname}"
-        cmd = ["wget", url, "--output-document", str(root / fname)]
-        subprocess.run(cmd, check=True, capture_output=True)
-        assert h_zip == sha256_file(
-            root / fname
-        ), f"Hash does not match: {fname}"
+        if not (root / fname).exists():
+            url = f"http://cs231n.stanford.edu/{fname}"
+            cmd = ["wget", url, "--output-document", str(root / fname)]
+            subprocess.run(cmd, check=True, capture_output=True)
+            assert h_zip == sha256_file(
+                root / fname
+            ), f"Hash does not match: {fname}"
+        else:
+            file_hash = sha256_file(root / fname)
+            if file_hash != h_zip:
+                (root / fname).unlink()
+                return get_imagenet(root)
 
+        if imagenet_dir.exists():
+            shutil.rmtree(imagenet_dir)
         with zipfile.ZipFile(root / fname) as zf:
             zf.extractall(root)
+
+        # add labels for val/
+        lbl_map = dict()
+        lbldirs = set()
+        with (imagenet_dir / "val/val_annotations.txt").open() as f:
+            for line in f.readlines():
+                imgname, dirname, *_ = line.split("\t")
+                lbl_map[imgname] = dirname
+                lbldirs.add(dirname)
+
+        [(imagenet_dir / "val" / d).mkdir() for d in lbldirs]
+        for imgfile in (imagenet_dir / "val/images").iterdir():
+            labeldir = lbl_map[imgfile.name]
+            imgfile.rename(imagenet_dir / "val" / labeldir / imgfile.name)
+        (imagenet_dir / "val/images").rmdir()
 
         assert imagenet_dir.exists()
         return imagenet_dir
@@ -77,17 +99,17 @@ def load_tiny_imagenet(root, **kwargs):
         path / "val",
         **kwargs,
     )
-    dataset_test = ImageFolder(
-        path / "test",
-        **kwargs,
-    )
+    # dataset_test = ImageFolder(
+    #     path / "test",
+    #     **kwargs,
+    # )
 
-    dataset_full = ConcatDataset([dataset_train, dataset_val, dataset_test])
+    dataset_full = ConcatDataset([dataset_train, dataset_val])
 
     # need to make a dataset that returns two transforms of an image
     # fmt: off
     T = TransformedPairDataset
-    test =                T(dataset_test,  transform_none)
+    test =                T(dataset_val,  transform_none)
     return dict(
         train_contrastive=T(dataset_full,  transform),
         train_linear     =T(dataset_train, transform_lin_train),
