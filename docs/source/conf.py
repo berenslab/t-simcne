f082dab18c24d0f4dd060ae734097ebde36655c7
Niklas Böhm <mail@jnboehm.com>
2024-12-15 13:04:02 +0100

    Bump version to 0.4.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 project = "tSimCNE"
 copyright = "2024, Nik Böhm"
 author = "Nik Böhm"
-release = "0.4.8"
+release = "0.4.9"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
