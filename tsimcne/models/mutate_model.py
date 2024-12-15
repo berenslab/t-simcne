da103a83e7d33f78b6b69ecec7a4ac49e250e13b
Niklas Böhm <mail@jnboehm.com>
2024-12-15 13:14:16 +0100

    Remove unnecesasry code from mutate_model.py

--- tsimcne/models/mutate_model.py
+++ tsimcne/models/mutate_model.py
@@ -3,7 +3,7 @@ from typing import Union
 import torch
 from torch import nn
 
-from .simclr_like import SimCLRModel, make_projection_head
+from .simclr_like import make_projection_head
 
 
 def mutate_model(
@@ -72,85 +72,3 @@ def mutate_model(
         )
 
     return model
-
-
-class FinetuneSimCLRModel(SimCLRModel):
-    def get_deps(self):
-        supdeps = super().get_deps()
-        return supdeps + [self.indir / "model.pt"]
-
-    def load(self):
-        self.state_dict = torch.load(
-            self.indir / "model.pt", map_location="cpu"
-        )
-        self.model = self.state_dict["model"]
-
-    def compute(self):
-        self.model = mutate_model(self.model, **self.kwargs)
-
-    def save(self):
-        # remove old values that might be present
-        self.state_dict.pop("model", None)
-        self.state_dict.pop("model_sd", None)
-
-        save_data = dict(
-            **self.state_dict,
-            model=self.model,
-            model_sd=self.model.state_dict(),
-        )
-        self.save_lambda_alt(self.outdir / "model.pt", save_data, torch.save)
-
-
-class ReadoutModel(FinetuneSimCLRModel):
-    """A model that is prepared for the linear readout stage.
-
-    Corresponds to the finetuned model with the following parameters:
-    ftmodel:change=proj_head:freeze=1:proj_head=linear and will
-    attempt to infer the out_dim, unless `out_dim` is passed
-    explicitly.
-
-    """
-
-    def __init__(
-        self,
-        path,
-        change="proj_head",
-        freeze="backbone",
-        proj_head="linear",
-        out_dim="infer",
-        **kwargs,
-    ):
-        super().__init__(
-            path,
-            change=change,
-            freeze=freeze,
-            proj_head=proj_head,
-            out_dim=out_dim,
-            **kwargs,
-        )
-
-    def get_deps(self):
-        supdeps = super().get_deps()
-        infer_outdim = self.kwargs["out_dim"] == "infer"
-
-        deps = (
-            (supdeps + [self.indir / "dataset.pt"])
-            if infer_outdim
-            else supdeps
-        )
-        return deps
-
-    def load(self):
-        super().load()
-
-        infer_outdim = self.kwargs["out_dim"] == "infer"
-        if infer_outdim:
-            data_dict = torch.load(self.indir / "dataset.pt")
-            dataset = data_dict["train_contrastive"]
-            try:
-                self.kwargs["out_dim"] = len(dataset.classes)
-            except AttributeError:
-                raise RuntimeError(
-                    "Cannot infer outdim from preceding dataset "
-                    f"and outdim was set to {self.kwargs['out_dim']}"
-                )
