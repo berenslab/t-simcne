bd321b93aa9f06ed5c16bc353baf5edf580a294d
Niklas Böhm <mail@jnboehm.com>
2025-01-10 14:00:01 +0100

    Bump version to 0.4.18

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 project = "tSimCNE"
 copyright = "2025, Nik Böhm"
 author = "Nik Böhm"
-release = "0.4.17"
+release = "0.4.18"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- tsimcne/__init__.py
+++ tsimcne/__init__.py
@@ -1,3 +1,3 @@
 from .tsimcne import PLtSimCNE, TSimCNE
 
-__version__ = "0.4.17"
+__version__ = "0.4.18"
